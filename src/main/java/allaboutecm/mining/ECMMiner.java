a88d2d65f6c1ba214845956806e2912dea3bb381
Farhad-Rezwan <frez0003@student.monash.edu>
2020-05-14 15:13:32 +1000

    adding test cases for the first assigned method of ECMMiner class

--- src/main/java/allaboutecm/mining/ECMMiner.java
+++ src/main/java/allaboutecm/mining/ECMMiner.java
@@ -7,10 +7,7 @@ import com.google.common.collect.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * TODO: implement and test the methods in this class.
@@ -90,7 +87,52 @@ public class ECMMiner {
      */
     public List<Musician> mostTalentedMusicians(int k) {
 
-        return Lists.newArrayList();
+        Collection<Musician> musicians = dao.loadAll(Musician.class);
+        Map<String, Musician> nameMap = Maps.newHashMap();
+        for (Musician m : musicians) {
+            nameMap.put(m.getName(), m);
+        }
+
+        ListMultimap<String, Album> multimap = MultimapBuilder.treeKeys().arrayListValues().build();
+        ListMultimap<Integer, Musician> countMap = MultimapBuilder.treeKeys().arrayListValues().build();
+
+        for (Musician musician : musicians) {
+            Set<Album> albums = musician.getAlbums();
+            for (Album album : albums) {
+                boolean toInclude = true;
+
+                if (toInclude) {
+                    multimap.put(musician.getName(), album);
+                }
+            }
+        }
+
+        Map<String, Collection<Album>> albumMultimap = multimap.asMap();
+        for (String name : albumMultimap.keySet()) {
+            Collection<Album> albums = albumMultimap.get(name);
+            int size = albums.size();
+            countMap.put(size, nameMap.get(name));
+        }
+
+        List<Musician> result = Lists.newArrayList();
+        List<Integer> sortedKeys = Lists.newArrayList(countMap.keySet());
+        sortedKeys.sort(Ordering.natural().reverse());
+        for (Integer count : sortedKeys) {
+            List<Musician> list = countMap.get(count);
+            if (list.size() >= k) {
+                break;
+            }
+            if (result.size() + list.size() >= k) {
+                int newAddition = k - result.size();
+                for (int i = 0; i < newAddition; i++) {
+                    result.add(list.get(i));
+                }
+            } else {
+                result.addAll(list);
+            }
+        }
+
+        return result;
     }
 
     /**
--- src/test/java/allaboutecm/mining/ECMMinerUnitTest.java
+++ src/test/java/allaboutecm/mining/ECMMinerUnitTest.java
@@ -3,7 +3,9 @@ package allaboutecm.mining;
 import allaboutecm.dataaccess.DAO;
 import allaboutecm.dataaccess.neo4j.Neo4jDAO;
 import allaboutecm.model.Album;
+import allaboutecm.model.MusicalInstrument;
 import allaboutecm.model.Musician;
+import allaboutecm.model.MusicianInstrument;
 import com.google.common.collect.Sets;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -39,5 +41,40 @@ class ECMMinerUnitTest {
 
         assertEquals(1, musicians.size());
         assertTrue(musicians.contains(musician));
+    }
+
+    @Test
+    public void shouldReturnTheMusicianWhenThereIsOnlyOneForTalentInInstrument() {
+        Album album = new Album(1975, "ECM 1064/65", "The Köln Concert");
+        Musician musician = new Musician ("Keith Jarrett");
+        musician.setAlbums(Sets.newHashSet(album));
+        when(dao.loadAll(Musician.class)).thenReturn(Sets.newHashSet(musician));
+
+        List<Musician> musicians = ecmMiner.mostTalentedMusicians(10);
+
+        assertEquals(1, musicians.size());
+        assertTrue(musicians.contains(musician));
+    }
+
+    public void shouldReturnMostTallentedMusicianIfHeHasMostInstrumentSkill() {
+        Album album = new Album(1975,"ECM 1064/65", "The Köln Concert");
+
+        MusicianInstrument musicianInstrument1 = new MusicianInstrument(new Musician("Keith Jarrett"),
+                Sets.newHashSet(new MusicalInstrument("Trumpet"), new MusicalInstrument("Effects"), new MusicalInstrument("Synthesizer")));
+        MusicianInstrument musicianInstrument2 = new MusicianInstrument(new Musician("Keith Jarrett"),
+                Sets.newHashSet(new MusicalInstrument("Trumpet"), new MusicalInstrument("Guitar"), new MusicalInstrument("Bass")));
+        MusicianInstrument musicianInstrument3 = new MusicianInstrument(new Musician("Avishai Cohen"),
+                Sets.newHashSet(new MusicalInstrument("Drums"), new MusicalInstrument("Live Sampling"), new MusicalInstrument("Piano")));
+        MusicianInstrument musicianInstrument4 = new MusicianInstrument(new Musician("Avishai Cohen"),
+                Sets.newHashSet(new MusicalInstrument("Accordion"), new MusicalInstrument("Clarinets"), new MusicalInstrument("Effects")));
+        MusicianInstrument musicianInstrument5 = new MusicianInstrument(new Musician("Vincent Courtois"),
+                Sets.newHashSet(new MusicalInstrument("Trumpet")));
+        MusicianInstrument musicianInstrument6 = new MusicianInstrument(new Musician("Uzi Ramirez"),
+                Sets.newHashSet(new MusicalInstrument("Clarinets"), new MusicalInstrument("Effects")));
+        MusicianInstrument musicianInstrument7 = new MusicianInstrument(new Musician("Pat Metheny"),
+                Sets.newHashSet(new MusicalInstrument("Synthesizer"), new MusicalInstrument("Guitar"), new MusicalInstrument("Bass")));
+
+        when(dao.loadAll(MusicianInstrument.class)).thenReturn(Sets.newHashSet(musicianInstrument1, musicianInstrument2, musicianInstrument3, musicianInstrument4, musicianInstrument5, musicianInstrument6, musicianInstrument7));
+
     }
 }
